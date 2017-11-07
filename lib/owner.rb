ee385466f24c0754df4879ae45cfebcf5d946839
Travis Petersen <travis.e.petersen@gmail.com>
2017-11-07 02:07:09 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,17 @@
 class Owner
-  # code goes here
+  attr_accessor :pets
+  @@all = []
+
+  def initialize
+    @pets = { fishes: [], cats: [], dogs: [] }
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
 end
